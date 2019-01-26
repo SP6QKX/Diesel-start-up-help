1c46f7d9f5494edab0e6b6512ae2a90259c2ab51
SP6QKX <sp6qkx@poczta.fm>
2019-01-26 15:41:48 +0100

    Update start_up.ino

--- start_up.ino
+++ start_up.ino
@@ -2,3 +2,5 @@
 // most olds diesel vehicle with use a small PIC12F508 processor.
 // Programmed to switching more resistance for 20 seconds makes 
 // the engine controller give a higher dose of fuel .
+
+
