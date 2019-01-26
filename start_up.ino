479af039eec91de902c5db8fa15d183b75c640ce
SP6QKX <sp6qkx@poczta.fm>
2019-01-26 14:07:31 +0100

    Update start_up.ino

--- start_up.ino
+++ start_up.ino
@@ -1,4 +1,4 @@
-Hallo Simple design that fix the problem with start-up 
-most olds diesel vehicle with use a small PIC12F508 processor.
-Programmed to switching more resistance for 20 seconds makes 
-the engine controller give a higher dose of fuel .
+// Hallo Simple design that fix the problem with start-up 
+// most olds diesel vehicle with use a small PIC12F508 processor.
+// Programmed to switching more resistance for 20 seconds makes 
+// the engine controller give a higher dose of fuel .
